7f94bcfdf57785aec11fd91a2cafa28f2c9be314
raman <ramanjit456@yahoo.co.in>
2016-09-10 10:58:57 -0400

    harambe speaks

--- background.js
+++ background.js
@@ -5,17 +5,31 @@
       'harambe2-transparent.png',
       'harambe3-transparent.png'
     ];
+
+    var harambeAdvice = [
+      "welcome!!"
+    ];
     var index = Math.floor(Math.random()*10) % 4;
+
     //Creating Elements
+    var cincinnatiZoo = document.createElement("div");
+    cincinnatiZoo.setAttribute("class", "dicks-out");
     var harambe = document.createElement("img")
-    harambe.setAttribute("class", "dicks-out");
+    harambe.setAttribute("class", "harambe");
     harambe.style.width = "200px";
     harambe.style.height = "200px";
     harambe.style.top = "0px";
     harambe.style.position = "fixed";
     var imgURL = chrome.extension.getURL("res/" + harambeEmotions[index]);
-    console.log(imgURL);
     harambe.src = imgURL;
 
+    var finalWords = document.createElement("p");
+    finalWords.style.top = "0px";
+    finalWords.style.position = "fixed";
+    finalWords.style.fontFamily = "Comic Sans MS";
+    finalWords.style.color = "magenta";
+    finalWords.innerHTML = harambeAdvice[0];
     //Appending to DOM
-    document.body.appendChild(harambe);
+    cincinnatiZoo.appendChild(harambe);
+    cincinnatiZoo.appendChild(finalWords);
+    document.body.appendChild(cincinnatiZoo);
