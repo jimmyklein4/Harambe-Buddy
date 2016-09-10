1163b07ccc398717e46c53ea0329622d63653a66
raman <ramanjit456@yahoo.co.in>
2016-09-10 11:58:01 -0400

    properly set zoo container to bottom right corner

--- background.js
+++ background.js
@@ -18,22 +18,24 @@
     //Creating Elements
     var cincinnatiZoo = document.createElement("div");
     cincinnatiZoo.setAttribute("class", "dicks-out");
+    cincinnatiZoo.style.width = "200px";
+    cincinnatiZoo.style.height = "200px";
+    cincinnatiZoo.style.top = "-webkit-calc(100% - 200px)";
+    cincinnatiZoo.style.left = "-webkit-calc(100% - 200px)";
+    cincinnatiZoo.style.position = "fixed";
+
     var harambe = document.createElement("img")
     harambe.setAttribute("class", "harambe");
     harambe.style.width = "200px";
     harambe.style.height = "200px";
-    harambe.style.top = "0px";
-    harambe.style.position = "fixed";
     var imgURL = chrome.extension.getURL("res/" + harambeEmotions[index]);
     harambe.src = imgURL;
 
     var finalWords = document.createElement("p");
-    finalWords.style.top = "0px";
-    finalWords.style.position = "fixed";
     finalWords.style.fontFamily = "Comic Sans MS";
     finalWords.style.color = "magenta";
     finalWords.innerHTML = harambeAdvice[0];
     //Appending to DOM
-    cincinnatiZoo.appendChild(harambe);
     cincinnatiZoo.appendChild(finalWords);
+    cincinnatiZoo.appendChild(harambe);
     document.body.appendChild(cincinnatiZoo);
