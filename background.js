1bc281c63b9186829590fa814e310b45b9a09cfe
raman <ramanjit456@yahoo.co.in>
2016-09-10 04:56:24 -0400

    harambe container made

---
+++ background.js
@@ -0,0 +1,10 @@
+
+    //Creating Elements
+    var harambe = document.createElement("div")
+    harambe.setAttribute("class", "dicks-out");
+    harambe.style.width = "200px";
+    harambe.style.height = "200px";
+    
+
+    //Appending to DOM
+    document.body.appendChild(harambe);
---
+++ content.js
@@ -0,0 +1,32 @@
+document.documentElement.style.height = '100%';
+document.body.style.height = '100%';
+document.documentElement.style.width = '100%';
+document.body.style.width = '100%';
+
+var div = document.createElement( 'div' );
+var btnForm = document.createElement( 'form' );
+var btn = document.createElement( 'input' );
+
+//append all elements
+document.body.appendChild( div );
+div.appendChild( btnForm );
+btnForm.appendChild( btn );
+//set attributes for div
+div.id = 'myDivId';
+div.style.position = 'fixed';
+div.style.top = '50%';
+div.style.left = '50%';
+div.style.width = '100%';
+div.style.height = '100%';
+div.style.backgroundColor = 'red';
+
+//set attributes for btnForm
+btnForm.action = '';
+
+//set attributes for btn
+//"btn.removeAttribute( 'style' );
+btn.type = 'button';
+btn.value = 'hello';
+btn.style.position = 'absolute';
+btn.style.top = '50%';
+btn.style.left = '50%';
