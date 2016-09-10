6faf80ca7adde50f4f41e3be799eb954f781712b
raman <ramanjit456@yahoo.co.in>
2016-09-10 05:17:21 -0400

    harambe is live

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
 
     //Creating Elements
-    var harambe = document.createElement("div")
+    var harambe = document.createElement("img")
     harambe.setAttribute("class", "dicks-out");
     harambe.style.width = "200px";
     harambe.style.height = "200px";
-    
+    harambe.style.top = "0px";
+    harambe.style.position = "fixed";
+    var imgURL = chrome.extension.getURL("res/harambe_transparent.png");
+    console.log(imgURL);
+    harambe.src = imgURL;
 
     //Appending to DOM
     document.body.appendChild(harambe);
